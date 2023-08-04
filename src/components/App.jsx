22cfcc0fa3579f64ce830c570a7b3b33ebaaeb36
PlutovaKate <114903556+PlutovaKate@users.noreply.github.com>
2023-08-04 19:25:31 +0300

    start homework

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,32 @@
-import { Component } from 'react';
+import { React, useEffect, useState } from 'react';
 import css from './App.module.css';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-export class App extends Component {
-  state = {
-    searchValue: '',
-  };
+const App = () => {
+  const [searchValue, setSearchValue] = useState('');
+};
 
-  handleFormSubmit = searchValue => {
-    this.setState({ searchValue });
-  };
+// export class App extends Component {
+//   state = {
+//     searchValue: '',
+//   };
 
-  render() {
-    return (
-      <div className={css.app}>
-        <Searchbar onSubmit={this.handleFormSubmit} />
-        <ImageGallery searchValue={this.state.searchValue} />
-        <ToastContainer autoClose={2000} position="top-right" theme="dark" />
-      </div>
-    );
-  }
-}
+//   handleFormSubmit = searchValue => {
+//     this.setState({ searchValue });
+//   };
+
+//   render() {
+//     return (
+//       <div className={css.app}>
+//         <Searchbar onSubmit={this.handleFormSubmit} />
+//         <ImageGallery searchValue={this.state.searchValue} />
+//         <ToastContainer autoClose={2000} position="top-right" theme="dark" />
+//       </div>
+//     );
+//   }
+// }
+
+export default App;
